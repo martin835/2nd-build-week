1c88121d7a8cfcd80bc317b2396f5921973b6192
martin835 <64438132+martin835@users.noreply.github.com>
2022-02-21 17:49:12 +0100

    resposivity,  dynamica profile image , text clamp

--- linkedin-react/src/App.js
+++ linkedin-react/src/App.js
@@ -1,32 +1,28 @@
+import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
-import ProfileList from "./components/ProfileList";
-
 import MyNavbar from "./components/MyNavbar";
 import SidebarEdit from "./components/SidebarEdit";
-
 import ProfileMainHero from './components/ProfileMainHero'
 import ProfileExperienceContainer from "./components/ProfileExperienceContainer";
-
+import ProfileList from "./components/ProfileList"
 
 
 function App() {
   return (
     <>
-      <MyNavbar />  
-
-      <Container className="mt-4">
-
+      <MyNavbar />
+      <Container>
         <Row>
-          <Col xs={9}>
-           <ProfileMainHero/>
-           <ProfileExperienceContainer/>
+          <Col xs={12} lg={9}>
+            <ProfileMainHero />
+            <ProfileExperienceContainer />
           </Col>
 
-          <Col xs={3}>
+          <Col className="d-none d-lg-block" lg={3}>
             <SidebarEdit />
-            <ProfileList />
+            <ProfileList/>
           </Col>
         </Row>
       </Container>
