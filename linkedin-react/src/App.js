42e414e5a4c6d9dec05a744906aaa6dc68a0eeb7
Ali Alaeddine <94364910+alalaed@users.noreply.github.com>
2022-02-21 12:35:38 +0100

    back ground color updated

--- linkedin-react/src/App.js
+++ linkedin-react/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
 import MyNavbar from "./components/MyNavbar";
+import SidebarEdit from "./components/SidebarEdit";
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
         <h1>LINKED IN</h1>
         <Row>
           <Col xs={9}>{/*  <ProfileMainSection/> */}</Col>
-          <Col xs={3}>{/*  <ProfileSideSection/> */}</Col>
+          <Col xs={3}>
+            <SidebarEdit />
+          </Col>
         </Row>
       </Container>
     </>
