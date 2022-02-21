7ea0e20afaa6c1c3d69a945ccf932fce6f2b4d8e
Ali Alaeddine <94364910+alalaed@users.noreply.github.com>
2022-02-21 15:44:50 +0100

    done

--- linkedin-react/src/App.js
+++ linkedin-react/src/App.js
@@ -1,31 +1,27 @@
-
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
 import ProfileList from "./components/ProfileList";
 
-
-
 import MyNavbar from "./components/MyNavbar";
 import SidebarEdit from "./components/SidebarEdit";
 import ProfileMainHero from "./components/ProfileMainHero";
 
-
 function App() {
   return (
-    <>   
-<Container className="mt-4">
-  <MyNavbar />
-  <Row>
-    <Col xs={9}>
-      <ProfileMainHero />
-    </Col>
-    <Col xs={3}>
-      <SidebarEdit />
-      <ProfileList />
-    </Col>
-  </Row>
-</Container>
+    <>
+      <MyNavbar />
+      <Container className="mt-4">
+        <Row>
+          <Col xs={9}>
+            <ProfileMainHero />
+          </Col>
+          <Col xs={3}>
+            <SidebarEdit />
+            <ProfileList />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
