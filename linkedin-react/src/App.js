07d1ad41ca041665966825c960bfde0a2fb0c2b7
Ali Alaeddine <94364910+alalaed@users.noreply.github.com>
2022-02-21 12:11:58 +0100

    Navbar done
    Icon in the search is still missing

--- linkedin-react/src/App.js
+++ linkedin-react/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {Container, Row, Col} from 'react-bootstrap'
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import MyNavbar from "./components/MyNavbar";
 
 function App() {
   return (
     <>
-     {/*  <MyNavbar/> */}
+      <MyNavbar />
       <Container>
         <h1>LINKED IN</h1>
         <Row>
-          <Col xs={9}>
-           {/*  <ProfileMainSection/> */}
-          </Col>
-          <Col xs={3}>
-           {/*  <ProfileSideSection/> */}
-          </Col>
+          <Col xs={9}>{/*  <ProfileMainSection/> */}</Col>
+          <Col xs={3}>{/*  <ProfileSideSection/> */}</Col>
         </Row>
       </Container>
-
     </>
   );
 }
