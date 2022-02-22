24a33fa805b41c7ae8719e2cad1ea09b845c59b1
martin835 <64438132+martin835@users.noreply.github.com>
2022-02-22 14:50:03 +0100

    editing of a job works

--- linkedin-react/src/components/OneExperienceCard.jsx
+++ linkedin-react/src/components/OneExperienceCard.jsx
@@ -1,6 +1,10 @@
 import { Container, Row, Col } from "react-bootstrap";
 
+
 function OneExperienceCard(props) {
+
+ 
+
   return (
     <>
       <Row className="mb-3">
@@ -8,21 +12,37 @@ function OneExperienceCard(props) {
           <div>
             <img
               className="martin-profile-experience-thumbnail-image mr-2"
-              src={props.image ? props.image : require("../assets/placeholder.jpeg")}
+              src={
+                props.image
+                  ? props.image
+                  : require("../assets/placeholder.jpeg")
+              }
             />
           </div>
         </Col>
         <Col xs={11}>
-          <h5 className="mb-0">{props.role}</h5>
-          <div>{props.company}</div>
-          <div className="text-secondary">{props.startDate} - {props.endDate ? props.endDate : "now" } 6 months</div>
-          <div className="text-secondary">{props.area}</div>
-          <p className="mt-2">
-            {props.description}
-          </p>
+          <Row>
+            <Col xs={10}>
+              <h5 className="mb-0">{props.role}</h5>
+              <div>{props.company}</div>
+              <div className="text-secondary">
+                {props.startDate} - {props.endDate ? props.endDate : "now"} 6
+                months
+              </div>
+              <div className="text-secondary">{props.area}</div>
+            </Col>
+            <Col xs={2}>
+              {props.editMode && (
+                <div className="martin-profile-icon-large mr-4" onClick={() => props.editJob(props.id)}>
+                  <i className="bi bi-pencil"></i>
+                </div>
+              )}
+            </Col>
+          </Row>
+          <p className="mt-2">{props.description}</p>
         </Col>
       </Row>
-      <hr/>
+      <hr />
     </>
   );
 }
--- linkedin-react/src/components/ProfileExperienceContainer.jsx
+++ linkedin-react/src/components/ProfileExperienceContainer.jsx
@@ -14,7 +14,12 @@ function ProfileExperienceContainer() {
     description: "",
     area: ""
   });
+  const [oneExperience, setOneExperience] = useState(null);
   const [lgShow, setLgShow] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const [idOfExperience, setIdOfExperience] = useState(null)
+
+ 
 
    useEffect(() => {
      loadExperiences();
@@ -52,9 +57,13 @@ function ProfileExperienceContainer() {
    const submitNewJob = async (e) => {
       e.preventDefault();
       console.log(newExperience);
+   
 
            let user = "62134b69be40b50015b6c935";
            //5fc4af46b708c200175de88f
+           
+          
+
            try {
              let response = await fetch(
                "https://striveschool-api.herokuapp.com/api/profile/" +
@@ -81,7 +90,85 @@ function ProfileExperienceContainer() {
            } catch (error) {
              console.log(error);
            }
-   } 
+   }
+   
+   const editJob = async (id) => {
+    setLgShow(true);
+    console.log(id);
+    setIdOfExperience(id);
+    
+    
+    console.log(newExperience);
+
+    let user = "62134b69be40b50015b6c935";
+    //5fc4af46b708c200175de88f
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/profile/" +
+          user +
+          "/experiences/" + id,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjEzNGI2OWJlNDBiNTAwMTViNmM5MzUiLCJpYXQiOjE2NDU0MzE2NTcsImV4cCI6MTY0NjY0MTI1N30.sW4qGqsabPColujp6kpA3P6pfCQ-VN9D8e5WEW1RdTI",
+          }
+        }
+      );
+      if (response.ok) {
+        let data = await response.json();
+        console.log(data);
+        setNewExperience({
+          role: data.role,
+          company: data.company,
+          startDate: data.startDate,
+          endDate: data.endDate,
+          description: data.description,
+          area: data.area,
+        });
+      } else {
+        alert("something went wrong :(");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+   }
+
+   const handleEdit = async () => {
+      let user = "62134b69be40b50015b6c935";
+      //5fc4af46b708c200175de88f
+
+      try {
+        let response = await fetch(
+          "https://striveschool-api.herokuapp.com/api/profile/" +
+            user +
+            "/experiences/" + idOfExperience,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjEzNGI2OWJlNDBiNTAwMTViNmM5MzUiLCJpYXQiOjE2NDU0MzE2NTcsImV4cCI6MTY0NjY0MTI1N30.sW4qGqsabPColujp6kpA3P6pfCQ-VN9D8e5WEW1RdTI",
+            },
+            body: JSON.stringify(newExperience),
+          }
+        );
+        if (response.ok) {
+          let data = await response.json();
+          console.log(data);
+          loadExperiences();
+          setLgShow(false);
+        } else {
+          alert("something went wrong :(");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+
+   }
+
+  
 
   return (
     <div className="p-3 bg-white mt-3 mb-5 martin-profile-experience-container">
@@ -93,7 +180,10 @@ function ProfileExperienceContainer() {
         >
           <i className="bi bi-plus-lg"></i>
         </div>
-        <div className="martin-profile-icon-large mr-4">
+        <div
+          className="martin-profile-icon-large mr-4"
+          onClick={() => setEditMode(!editMode)}
+        >
           <i className="bi bi-pencil"></i>
         </div>
       </div>
@@ -101,8 +191,10 @@ function ProfileExperienceContainer() {
         experiences.map((experience) => (
           <OneExperienceCard
             key={experience._id}
+            editJob={editJob}
+            editMode={editMode}
             role={experience.role}
-            id={experience.id}
+            id={experience._id}
             company={experience.company}
             startDate={experience.startDate}
             endData={experience.endDate}
@@ -115,7 +207,17 @@ function ProfileExperienceContainer() {
       <Modal
         size="lg"
         show={lgShow}
-        onHide={() => setLgShow(false)}
+        onHide={() => { setEditMode(!editMode);          
+          setLgShow(false);
+          setNewExperience({
+            role: "",
+            company: "",
+            startDate: "",
+            endDate: null,
+            description: "",
+            area: "",
+          });
+        }}
         aria-labelledby="example-modal-sizes-title-lg"
       >
         <Modal.Header closeButton>
@@ -210,19 +312,33 @@ function ProfileExperienceContainer() {
                 type="datetime-local"
                 value={newExperience.endDate}
                 onChange={(e) =>
-                  setNewExperience({ ...newExperience, endDate: e.target.value })
+                  setNewExperience({
+                    ...newExperience,
+                    endDate: e.target.value,
+                  })
                 }
               />
             </Form.Group>
           </Modal.Body>
           <Modal.Footer>
-            <Button
-              variant="primary"
-              type="submit"
-              className="martin-profile-main-btn mb-2 mb-lg-0"
-            >
-              Save
-            </Button>
+            {editMode ? (
+              <Button
+                variant="primary"
+                type="button"
+                className="martin-profile-main-btn mb-2 mb-lg-0"
+                onClick={()=>handleEdit()}
+              >
+                Edit Job
+              </Button>
+            ) : (
+              <Button
+                variant="primary"
+                type="submit"
+                className="martin-profile-main-btn mb-2 mb-lg-0"
+              >
+                Save
+              </Button>
+            )}
           </Modal.Footer>
         </Form>
       </Modal>
