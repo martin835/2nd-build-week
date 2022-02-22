7eca5d6b58a8f6128c04159aa90862af5bb0d7f8
Oluwagbemi Oluwasakin <51876261+Gbemi06@users.noreply.github.com>
2022-02-22 15:03:23 +0200

    People you know container is done

--- linkedin-react/src/components/ProfileCard.jsx
+++ linkedin-react/src/components/ProfileCard.jsx
@@ -1,18 +1,24 @@
 import { Row, Col, Button } from "react-bootstrap";
 
-const ProfileCard = () => {
+const ProfileCard = ({ profiles }) => {
   return (
     <Row>
       <Col xs={3}>
         <img
           className="Gbemi-image1"
-          src={require("../assets/user.png")}
+          src={
+            profiles.image
+              ? profiles.image
+              : require("../assets/placeholder.jpeg")
+          }
           alt="profile pics"
         />
       </Col>
       <Col xs={9}>
-        <h6 className="mb-0 Gbemi-h6">Martina Jenny </h6>
-        <p className="mb-1 Gbemi-profile-card-description">lorem ipsum 50 </p>
+        <h6 className="mb-0 Gbemi-h6">
+          {profiles.name} {profiles.surname}{" "}
+        </h6>
+        <p className="mb-1 Gbemi-profile-card-description">{profiles.title} </p>
         <Button className="Gbemi-button1" variant="outline-secondary">
           Connect
         </Button>
