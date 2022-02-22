24dd517fed7a2c90425dbfc2682d289c27daaf41
martin835 <64438132+martin835@users.noreply.github.com>
2022-02-22 12:04:26 +0100

    posting works

--- linkedin-react/src/components/OneExperienceCard.jsx
+++ linkedin-react/src/components/OneExperienceCard.jsx
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 
-function OneExperienceCard() {
+function OneExperienceCard(props) {
   return (
     <>
       <Row className="mb-3">
@@ -8,19 +8,17 @@ function OneExperienceCard() {
           <div>
             <img
               className="martin-profile-experience-thumbnail-image mr-2"
-              src={require("../assets/strive-logo.jpeg")}
+              src={props.image ? props.image : require("../assets/placeholder.jpeg")}
             />
           </div>
         </Col>
         <Col xs={11}>
-          <h5 className="mb-0">Full Stack Developer in making</h5>
-          <div>Strive School</div>
-          <div className="text-secondary">Nov 2021- May 2022 6 months</div>
-          <div className="text-secondary">Bratislava/Berlin</div>
+          <h5 className="mb-0">{props.role}</h5>
+          <div>{props.company}</div>
+          <div className="text-secondary">{props.startDate} - {props.endDate ? props.endDate : "now" } 6 months</div>
+          <div className="text-secondary">{props.area}</div>
           <p className="mt-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ex
-            nihil dolorum soluta vero odio, voluptas neque omnis quas tenetur
-            quae voluptatibus ullam est id ipsum alias. Vel, laudantium earum.
+            {props.description}
           </p>
         </Col>
       </Row>
