e01766c0629d0ba2d9a8e72845bb2f9b1c56ece6
martin835 <64438132+martin835@users.noreply.github.com>
2022-02-23 12:18:13 +0100

    three dots added

--- linkedin-react/src/components/MyOnePost.jsx
+++ linkedin-react/src/components/MyOnePost.jsx
@@ -2,12 +2,15 @@ import { Row, Col } from "react-bootstrap";
 
 function MyOnePost(props) {
   return (
-     <Row>
-         <Col>
-         {props.text} 
-         </Col>
-     </Row>
-  )
+    <Row>
+      <Col>
+        {props.text}{" "}
+        <span className="float-right martin-profile-icon-large">
+          <i class="bi bi-three-dots"></i>
+        </span>
+      </Col>
+    </Row>
+  );
 }
 
 export default MyOnePost;
