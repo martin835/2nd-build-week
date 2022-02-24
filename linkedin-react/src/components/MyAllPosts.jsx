d8bcfcb7929cb87231e6495f32af05d22920d03a
Ali Alaeddine <94364910+alalaed@users.noreply.github.com>
2022-02-24 16:20:11 +0100

    my posts

--- linkedin-react/src/components/MyAllPosts.jsx
+++ linkedin-react/src/components/MyAllPosts.jsx
@@ -1,43 +1,40 @@
 import { Row, Col, Spinner } from "react-bootstrap";
 import MyOnePost from "./MyOnePost";
-import {useState, useEffect} from "react"
-
-
+import { useState, useEffect } from "react";
 
 function MyAllPosts() {
+  const [allPosts, setAllPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    loadMyPosts();
+  }, []);
 
-const [allPosts, setAllPosts] = useState([])
-const [loading, setLoading] = useState(false);
-useEffect(()=>{loadMyPosts()},[])
-
-const loadMyPosts = async () => {
-         
-     try {
+  const loadMyPosts = async () => {
+    try {
       setLoading(true);
-       let response = await fetch(
-         "https://striveschool-api.herokuapp.com/api/posts/",
-         {
-           method: "GET",
-           headers: {
-             "Content-Type": "application/json",
-             Authorization:
-               "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjEzNGI2OWJlNDBiNTAwMTViNmM5MzUiLCJpYXQiOjE2NDU0MzE2NTcsImV4cCI6MTY0NjY0MTI1N30.sW4qGqsabPColujp6kpA3P6pfCQ-VN9D8e5WEW1RdTI",
-           },
-         }
-       );
-       if (response.ok) {
-         let data = await response.json();
-    
-         setAllPosts(data);
-         setLoading(false);
-       } else {
-         alert("something went wrong :(");
-       }
-     } catch (error) {
-       console.log(error);
-     }
-   };
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/posts/",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjEzNGI2OWJlNDBiNTAwMTViNmM5MzUiLCJpYXQiOjE2NDU0MzE2NTcsImV4cCI6MTY0NjY0MTI1N30.sW4qGqsabPColujp6kpA3P6pfCQ-VN9D8e5WEW1RdTI",
+          },
+        }
+      );
+      if (response.ok) {
+        let data = await response.json();
 
+        setAllPosts(data);
+        setLoading(false);
+      } else {
+        alert("something went wrong :(");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="p-3 bg-white mt-3 mb-5 martin-profile-experience-container">
@@ -48,6 +45,7 @@ const loadMyPosts = async () => {
           .filter((post) => post.username === "Martin1234")
           .map((post) => (
             <MyOnePost
+              post={post}
               loadMyPosts={loadMyPosts}
               text={post.text}
               postId={post._id}
